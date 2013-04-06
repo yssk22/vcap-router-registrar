29bc584e61d0217d0aa3c8b1bf7eb49aabef1c05
yssk22 <yssk22@gmail.com>
2013-04-06 20:10:11 +0900

    pifile capability added

--- lib/router_registrar.rb
+++ lib/router_registrar.rb
@@ -11,7 +11,7 @@ module RouterRegistrar
 
   class Config
     class << self
-      [:logger, :nats_uri, :uri, :host, :port, :tags].each { |option| attr_accessor option }
+      [:logger, :nats_uri, :uri, :host, :port, :tags, :pid].each { |option| attr_accessor option }
 
       def configure(config)
         VCAP::Logging.setup_from_config(config["logging"])
@@ -23,6 +23,7 @@ module RouterRegistrar
         @host = config["host"]
         @port = config["port"]
         @tags = config["tags"]
+        @pid  = config["pid"]
       end
     end
   end
@@ -34,6 +35,8 @@ module RouterRegistrar
 
     def initialize
       @logger = Config.logger
+      
+      create_pidfile
 
       @registration_message = Yajl::Encoder.encode({
         :host => Config.host,
@@ -71,6 +74,15 @@ module RouterRegistrar
       @nats.publish(ROUTER_UNREGISTER_TOPIC, @registration_message, &block)
     end
 
+    def create_pidfile
+      begin
+        pid_file = VCAP::PidFile.new(Config.pid)
+        pid_file.unlink_at_exit
+      rescue => e
+        $stderr.puts "ERROR: Can't create pid file #{Config.pid}"
+        exit 1
+      end
+    end
   end
 
 end
